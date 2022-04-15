193301a0f111abec8702834569260a97ea634518
kimgoesflying <yelizarova.ae@gmail.com>
2022-04-16 02:09:08 +0300

    add models methods

--- news/models.py
+++ news/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import Sum
 # Create your models here.
 
 
 class Author(models.Model):
     username = models.OneToOneField(
         User, on_delete=models.CASCADE)
-    raiting = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = Post.objects.filter(author_id=self.pk).aggregate(
+            Sum('rating'))['rating__sum'] * 3
+
+        comment_rating = Comment.objects.filter(username_id=self.username).aggregate(
+            Sum('rating'))['rating__sum']
+
+        # post_comment_rating =
+
+        self.rating = post_rating + comment_rating
+        self.save()
 
 
 class Category(models.Model):
@@ -37,6 +46,18 @@ class Post(models.Model):
     category = models.ManyToManyField(
         Category, through='PostCategory', blank=True)
 
+    def preview(self):
+        preview = f'{self.text[0:124]} ...'
+        return preview
+
+    def like(self):
+        self.rating = self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating = self.rating - 1
+        self.save()
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -50,3 +71,11 @@ class Comment(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating = self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating = self.rating - 1
+        self.save()
